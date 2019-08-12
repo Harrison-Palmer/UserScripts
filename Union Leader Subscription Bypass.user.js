8043bee31a9561b6ec8945a34dcf13b2506364dc
Harrison Palmer <hpalmer@student.dwc.edu>
2019-08-12 13:29:37 -0400

    Add files via upload

---
+++ Union Leader Subscription Bypass.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Union Leader Subscription Bypass
+// @namespace    http://tampermonkey.net/
+// @require http://code.jquery.com/jquery-3.4.1.min.js
+// @version      1.0
+// @description  Subscription Bypass
+// @author       Harrison Palmer
+// @match        https://www.unionleader.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Close popup.
+    $('.close').click();
+
+    // Delete Popup.
+    $('.modal-content').remove();
+
+    // Remove Preview.
+    $('.subscriber-preview').css('position','');
+
+    // Remove login requirement box.
+    $('.subscription-required').remove();
+
+    // Show rest of article.
+    $('.subscriber-only').removeClass();
+})();
